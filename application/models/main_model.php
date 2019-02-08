01ea6268eaad3978e046139af6e3d4ac2b887d8c
Florianmath <43069283+Florianmath@users.noreply.github.com>
2019-02-08 15:35:15 +0100

    Add files via upload

--- application/models/main_model.php
+++ application/models/main_model.php
@@ -20,7 +20,7 @@ class Main_model extends CI_Model {
         $code="aut";
         */
         //Requete
-        $sql=$this->db->conn_id->prepare ("SELECT  login, IdLot, nomBateau, nomEspece, specification, libellePresentation, LibelleQualite, poidsBrutLot, prixDepart, prixEncheresMax, dateEnchere, dateHeureFin, codeEtat, prixActuel, nbreJourLot FROM acheteur, lot, bateau, espece, peche, taille, presentation, qualite WHERE lot.IdAcheteur = acheteur.IdAcheteur AND bateau.IdBateau = lot.IdBateau AND espece.IdEspece = lot.IdEspece AND taille.IdTaille = lot.IdTaille AND presentation.IdPresentation = lot.IdPresentation AND qualite.IdQualite = lot.IdQualite AND codeEtat='en cours'");
+        $sql=$this->db->conn_id->prepare ("SELECT DISTINCT  login, IdLot, nomBateau, nomEspece, specification, libellePresentation, LibelleQualite, poidsBrutLot, prixDepart, prixEncheresMax, dateEnchere, dateHeureFin, codeEtat, prixActuel, nbreJourLot FROM acheteur, lot, bateau, espece, peche, taille, presentation, qualite WHERE lot.IdAcheteur = acheteur.IdAcheteur AND bateau.IdBateau = lot.IdBateau AND espece.IdEspece = lot.IdEspece AND taille.IdTaille = lot.IdTaille AND presentation.IdPresentation = lot.IdPresentation AND qualite.IdQualite = lot.IdQualite AND codeEtat='en cours'");
             /*(SELECT DATEDIFF(day, '2014-01-09', '2014-01-01');)*/
         $sql->execute();
         $donnees=$sql->fetchAll();//Execution total
@@ -30,16 +30,17 @@ class Main_model extends CI_Model {
     }
 
     public function afficheEnchereProgrammee(){
-         $sql3=$this->db->conn_id->prepare ("SELECT  login, IdLot, nomBateau, nomEspece, specification, libellePresentation, LibelleQualite, poidsBrutLot, prixDepart, prixEncheresMax, dateEnchere, dateHeureFin, codeEtat, prixActuel, nbreJourLot FROM acheteur, lot, bateau, espece, peche, taille, presentation, qualite WHERE lot.IdAcheteur = acheteur.IdAcheteur AND bateau.IdBateau = lot.IdBateau AND espece.IdEspece = lot.IdEspece AND taille.IdTaille = lot.IdTaille AND presentation.IdPresentation = lot.IdPresentation AND qualite.IdQualite = lot.IdQualite AND codeEtat='programmée'");
+         $sql3=$this->db->conn_id->prepare ("SELECT DISTINCT  login, IdLot, nomBateau, nomEspece, specification, libellePresentation, LibelleQualite, poidsBrutLot, prixDepart, prixEncheresMax, dateEnchere, dateHeureFin, codeEtat, prixActuel, nbreJourLot FROM acheteur, lot, bateau, espece, peche, taille, presentation, qualite WHERE lot.IdAcheteur = acheteur.IdAcheteur AND bateau.IdBateau = lot.IdBateau AND espece.IdEspece = lot.IdEspece AND taille.IdTaille = lot.IdTaille AND presentation.IdPresentation = lot.IdPresentation AND qualite.IdQualite = lot.IdQualite AND codeEtat='programmée'");
         $sql3->execute();
         $programmee=$sql3->fetchAll();
         return $programmee;
     }
 
 
+
    /* FONCTION D'AFFICHAGE : Permet d'afficher dans l'onglet Administrateur les enchères terminées : même chose que les autres sauf qu'on remplace le codeEtat */ 
     public function afficheEnchereTerminee(){
-        $sql16=$this->db->conn_id->prepare ("SELECT login, IdLot, nomBateau, nomEspece, specification, libellePresentation, LibelleQualite, poidsBrutLot, prixDepart, prixEncheresMax, dateEnchere, dateHeureFin, codeEtat, prixActuel FROM lot, bateau, espece, peche, taille, presentation, qualite, acheteur WHERE acheteur.IdAcheteur = lot.IdAcheteur AND bateau.IdBateau = lot.IdBateau AND espece.IdEspece = lot.IdEspece AND taille.IdTaille = lot.IdTaille AND presentation.IdPresentation = lot.IdPresentation AND qualite.IdQualite = lot.IdQualite AND codeEtat ='terminé'");
+        $sql16=$this->db->conn_id->prepare ("SELECT DISTINCT login, IdLot, nomBateau, nomEspece, specification, libellePresentation, LibelleQualite, poidsBrutLot, prixDepart, prixEncheresMax, dateEnchere, dateHeureFin, codeEtat, prixActuel FROM lot, bateau, espece, peche, taille, presentation, qualite, acheteur WHERE acheteur.IdAcheteur = lot.IdAcheteur AND bateau.IdBateau = lot.IdBateau AND espece.IdEspece = lot.IdEspece AND taille.IdTaille = lot.IdTaille AND presentation.IdPresentation = lot.IdPresentation AND qualite.IdQualite = lot.IdQualite AND codeEtat ='terminé' AND IdFacture = 0");
         $sql16->execute();
         $enchereterminee=$sql16->fetchAll();
         return $enchereterminee;
@@ -62,7 +63,7 @@ class Main_model extends CI_Model {
 /* FONCTION D'AFFICHAGE : Permet d'afficher les utilisateurs : même chose qu'affiche administrateur, on met juste un where pour tout afficher sauf les admins. ATTENTION : si on met en place un nouveau choix qui n'est pas ni administrateur ni utilisateur, cela va l'afficher, à changer si cela arrive */ 
 
     public function afficheUtilisateur(){
-        $sql18=$this->db->conn_id->prepare ("SELECT DISTINCT login FROM acheteur WHERE login <> 'administrateur'");
+        $sql18=$this->db->conn_id->prepare ("SELECT DISTINCT login FROM acheteur WHERE login <> 'administrateur' AND login <> 'Personne'");
         $sql18->execute();
         $compte_utilisateur=$sql18->fetchAll();
         return $compte_utilisateur;
@@ -118,9 +119,17 @@ class Main_model extends CI_Model {
 
     public function recupAcheteur()
     {
-        $sql12=$this->db->conn_id->prepare("SELECT acheteur.IdAcheteur FROM acheteur");
+        $sql12=$this->db->conn_id->prepare("SELECT acheteur.IdAcheteur FROM acheteur WHERE IdAcheteur = 0");
         $sql12->execute();
         $acheteur = $sql12->fetchAll();
+        return $acheteur;
+    }
+
+    public function recupLogin()
+    {
+        $sql30=$this->db->conn_id->prepare("SELECT acheteur.login FROM acheteur");
+        $sql30->execute();
+        $acheteur = $sql30->fetchAll();
         return $acheteur;
     }
 
@@ -132,6 +141,60 @@ class Main_model extends CI_Model {
         return $qualite;
     }
 
+    public function recupLot()
+    {
+        $sql21=$this->db->conn_id->prepare("SELECT lot.IdLot FROM lot WHERE codeEtat = 'terminé' AND IdFacture = 0");
+        $sql21->execute();
+        $recupLot = $sql21->fetchAll();
+        return $recupLot;
+    }
+
+    public function recupFacture()
+    {
+        $sql18=$this->db->conn_id->prepare("SELECT lot.IdFacture FROM lot");
+        $sql18->execute();
+        $facture = $sql18->fetchAll();
+        return $facture;
+    }
+
+    public function afficheDateEnchere(){
+
+      /*  $sql23=$this->db->conn_id->prepare ("SELECT IdLot FROM lot order by desc "); // on prend le nombre de lots tt simplement
+        $sql23->execute();
+        $retourneLot=$sql23->fetchAll();*/
+
+        $sql22=$this->db->conn_id->prepare ("SELECT IdLot, dateEnchere FROM lot"); // on prend le nbre de jour de IdLot num 3 (=12)
+        $sql22->execute();
+        $retourneDateE=$sql22->fetchAll();
+
+        $dateDuJour = date('Y-m-d');
+        $dateActuelle = strtotime($dateDuJour); 
+
+            foreach ($retourneDateE as $row)
+             {
+                $getLot = $row['IdLot'];
+                $dateEnchere = strtotime($row['dateEnchere']);
+                $gmdate1 = ceil(abs($dateEnchere - $dateActuelle) / 86400);
+                $this->db->set('nbreJourLot', $gmdate1);
+                $this->db->where('IdLot', $getLot);
+                $this->db->update('lot');
+             }
+              
+            
+        }
+        
+         
+        
+
+        
+
+        
+       
+         // ---- AFFICHE 31, c'est OK pour lot 1 ----- 
+        
+    
+        
+        
 
 
     public function lesreferences()
@@ -147,6 +210,11 @@ class Main_model extends CI_Model {
     public function enregistreAcheteur($data)
     {
         $this->db->insert('acheteur', $data);
+    }
+
+    public function facture($lafacture)
+    {
+        $this->db->insert('facture', $lafacture);
     }
 
     public function enregistreBateau($tab)
@@ -167,8 +235,6 @@ class Main_model extends CI_Model {
     public function enregistreLot($tab)
     {
         $this->db->insert('lot', $tab);
-        //$req = $bdd->prepare('INSERT INTO lot(IdLot, poidsBrutLot, prixPlancher, prixDepart, prixActuel, prixEncheresMax, dateEnchere, dateHeureFin, codeEtat, IdFacture) VALUES(:IdLot, :poidsBrutLot, :prixPlancher, :prixDepart, :prixActuel, :prixEncheresMax, :dateEnchere, :dateHeureFin, :codeEtat, :IdFacture)');
-        
     }
 
     public function enregistrePresentation($tab)
@@ -176,8 +242,46 @@ class Main_model extends CI_Model {
         $this->db->insert('presentation', $tab);
     }
 
+    public function enregistreNouvelleNumRue($numRue)
+    {
+        $this->db->set('numRue', $numRue);
+        $this->db->where('IdAcheteur', 3);
+        $this->db->update('acheteur');
+    }
+
+    public function enregistreNouvelleRue($rue)
+    {
+        $this->db->set('rue', $rue);
+        $this->db->where('IdAcheteur', 3);
+        $this->db->update('acheteur');
+    }
+
+    public function enregistreNouvelleVille($ville)
+    {
+        $this->db->set('ville', $ville);
+        $this->db->where('IdAcheteur', 3);
+        $this->db->update('acheteur');
+    }
+
+    public function enregistreNouvelleCodePostal($codePostal)
+    {
+        $this->db->set('codePostal', $codePostal);
+        $this->db->where('IdAcheteur', 3);
+        $this->db->update('acheteur');
+    }
+
+    public function enregistreNouvelleNumHabilitation($numHabilitation)
+    {
+        $this->db->set('numHabilitation', $numHabilitation);
+        $this->db->where('IdAcheteur', 3);
+        $this->db->update('acheteur');
+    }
 
 
+
+
+    
+    
 
     public function enregistrePrix($nouveauPrix, $idlot)
     {
@@ -186,11 +290,6 @@ class Main_model extends CI_Model {
       $this->db->update('lot');
     }
 
-    public function enregistreDateEnchere($nouvelleDate)
-    {
-        $this->Db->set('dateEnchere', $nouvelleDate);
-        $this->db->update('lot');
-    }
 
     public function userLogin($login,$pwd)
     {
@@ -203,19 +302,22 @@ class Main_model extends CI_Model {
                     ->result();
     }
 
+
     public function etatEnchere($gmdate)
     {
       $this->db->set('codeEtat', 'terminé');
       $this->db->where('dateHeureFin <', $gmdate);
       $this->db->update('lot');
     }
-/* Tentative de mettre en place le nombre de lot. Pas de where car pas d'obligations particulières,
-    Problème : relier dateEnchere de la BDD à une variable $dateEnchere */
 
-    public function nombreLot($gmdate1)
+    public function recupEnchere()
     {
-        $this->db->set('nbreJourLot', $gmdate1);
-        $this->db->update('lot');
+        $sql20=$this->db->conn_id->prepare("UPDATE lot SET codeEtat = 'en cours' ORDER BY `IdLot` DESC LIMIT 1");
+        $sql20->execute();
+        $etatEnchere=$sql20->fetchAll();//Execution total
+        //ferme la connexion
+        //$this->db=null;
+        return $etatEnchere;
     }
 
 /* ----- FONCTIONS ETAT : Permettent de mettre en place la qualité en direct ----- */
@@ -255,6 +357,184 @@ class Main_model extends CI_Model {
         $this->db->update('lot');
     }
 
+    public function suppressionLot($unIdLot)
+    {
+        $this->db->where('IdLot', $unIdLot);
+        $this->db->delete('lot');
+       // $this->db->insert('lot', $tab);
+    }
+    
+    public function validationLot($facture)
+    {
+        $this->db->insert('facture', $facture);
+
+    }
+ // ----- MISE EN PLACE DU CHAT - TROISIEME VERSION OK ----- //
+     public function recupMessage()
+    {
+         $sql1=$this->db->conn_id->prepare ("SELECT login, minichat.message, minichat.dateHeure FROM acheteur, minichat WHERE minichat.IdAcheteur = acheteur.IdAcheteur AND minichat.message  <> '' ORDER BY id DESC LIMIT 0, 10");
+         $sql1->execute();
+         $recupMessage=$sql1->fetchAll();
+         return $recupMessage;
+    }
+
+    public function insertMessage($loginAcheteur, $mesMessages)
+     {
+       $this->db->select('IdAcheteur')
+       ->from('acheteur')
+       ->where('login', $loginAcheteur);
+       $query = $this->db->get();
+       foreach($query->result() as $row)
+       {
+         //   $dateDuJour = date('Y-m-d');
+            $IdAcheteur = $row->IdAcheteur;
+            $this->db->set('IdAcheteur', $IdAcheteur);
+            
+        
+         //   $this->db->where('id', $id);
+      //      $this->db->where('id', $id);
+           // $this->db->update('minichat');
+           
+       }
+       $this->db->insert('minichat', $mesMessages);
+   }
+
+
+       
+
+   public function factures($IdFacture, $idlot)
+   {
+     
+ //   $this->db->insert('facture', $IdFacture);
+     $this->db->set('IdFacture', $IdFacture);
+     $this->db->where('IdLot', $idlot);
+     $this->db->update('lot');
+
+   }
+   
+    public function enregistreIdAcheteur($loginAcheteur, $idlot)
+{
+  $this->db->select('IdAcheteur')
+  ->from('acheteur')
+  ->where('login', $loginAcheteur);
+   $query = $this->db->get();
+   foreach ($query->result() as $row)
+    {
+    $IdAcheteur = $row->IdAcheteur;
+    $this->db->set('IdAcheteur', $IdAcheteur);
+    $this->db->where('IdLot', $idlot);
+    $this->db->update('lot');
+    }
+}
+ // ----- MISE EN PLACE DU CHAT - DEUXIEME VERSION OK -----
+  /*  public function recupMessage()
+    {
+         $sql1=$this->db->conn_id->prepare ("SELECT login, message FROM acheteur WHERE message <> '' ORDER BY login DESC LIMIT 0, 10");
+         $sql1->execute();
+         $recupMessage=$sql1->fetchAll();
+         return $recupMessage;
+    }
+
+    public function insertMessage($mesMessages)
+   {
+    $this->db->insert('acheteur', $mesMessages);
+   }*/
+
+/* ----- MISE EN PLACE DU CHAT - PREMIERE VERSION OK ----- 
+   public function recupMessage()
+   {
+     $sql1=$this->db->conn_id->prepare ("SELECT pseudo, message FROM minichat ORDER BY ID ASC LIMIT 0, 10");
+     $sql1->execute();
+     $recupMessage=$sql1->fetchAll();
+     return $recupMessage;
+   }
+
+   public function insertMessage($mesMessages)
+   {
+    $this->db->insert('minichat', $mesMessages);
+   }*/
+    
+// ---- AFFICHAGE DES INFOS DE L'UTILISATEUR : PAS OK ----- //
+    public function afficheInfoUtilisateur()
+    {
+         $sql59=$this->db->conn_id->prepare("SELECT rue, numRue, ville, codePostal, numHabilitation FROM acheteur where IdAcheteur = 3");
+         $sql59->execute();
+         $resultats=$sql59->fetchAll();
+         return $resultats;
+    }
+
+/*public function afficheInfoUtilisateur()
+    {
+         $sql44=$this->db->conn_id->prepare("SELECT login, pwd, rue, numRue, ville, codePostal, numHabilitation FROM acheteur WHERE IdAcheteur = 2 ");
+        $sql44->execute();
+        $infoUser=$sql44->fetchAll();//Execution total
+        //ferme la connexion
+        //$this->db=null2
+        return $infoUser;
+       /* $this->db->select('rue, numRue, ville, codePostal, numHabilitation');
+        $this->db->from('acheteur');
+        $this->db->where('login', $login);
+        $this->db->where('pwd', $pwd);
+        $query = $this->db->get();
+        return $query;*/
+    // -----  TENTATIVE DE METTRE EN PLACE SYSTEME CHAT PAS TOUCHE -----  //
+  
+   /* function add_message($message, $pseudonyme, $guide)
+    {
+        $data = array(
+            'message'   => (string) $message,
+            'pseudonyme'  => (string) $pseudonyme,
+            'guide'      => (string) $guide,
+            'timestamp' => time(),
+        );
+          
+        $this->db->insert('message', $data);
+    }
+ 
+    function get_messages($timestamp)
+    {
+        $this->db->where('timestamp >', $timestamp);
+        $this->db->order_by('timestamp', 'DESC');
+        $this->db->limit(10); 
+        $query = $this->db->get('message');
+        
+        return array_reverse($query->result_array());
+    }*/
+ 
+
+
+
+    // ----- POUBELLE -----  //  
+
+  /*  public function relierLot($unefacture)
+    {
+        $sql26=$this->db->conn_id->prepare("SELECT IdLot FROM lot");
+        $sql26->execute();
+        $IdLot=$sql26->fetchAll();
+
+        $this->db->set('IdFacture', $unefacture);
+        $this->db->where('IdLot', $IdLot);
+        $this->db->update('lot');*/
+      /*  $this->db->insert('lot', $unefacture);
+        $this->db->insert('presentation', $tab);  
+    }*/
+
+    /*public function recupLibelleFacture()
+    {
+        $sql38=$this->db->conn_id->prepare("SELECT libelleFacture from facture");
+        $sql38->execute();
+        $libelleFacture=$sql38->fetchAll();//Execution total
+        //ferme la connexion
+        //$this->db=null;
+        return $libelleFacture;
+    }*/
+    /*public function validationLot($idfacture)
+    {
+        $this->db->insert('facture', $idfacture);
+       
+    }*/
+// $this->db->delete('mytable', array('id' => $id))
+
 
  /*   public function taillePetite($taille1)
     {
@@ -279,15 +559,5 @@ class Main_model extends CI_Model {
     }*/
 /* ----- FIN FONCTIONS ETAT ----- */
 
-
- 
-// Tentative de mettre en place le "meilleur acheteur", càd celui qui arrive à acheter l'enchère terminée, pas réussi pour l'instant, à voir car incomplet actuellement. 
- /*public function afficheMeilleurAcheteur()
-    {
-        $sql17=$this->db->conn_id->prepare("SELECT DISTINCT login WHERE acheteur.IdAcheteur = lot.IdAcheteur AND MAX(prixActuel) = prixActuel");
-        $sql17->execute();
-        $meilleuracheteur=$sql17->fetchAll();
-        return $meilleuracheteur;
-    }*/
-}  
+}
 ?>
---
+++ application/views/v_monCompte.php
@@ -0,0 +1,91 @@
+<style type="text/css">
+.footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+           }
+#logoCompte {
+  color: #000000;
+  margin-right: 20px;
+}
+</style>
+
+<h3> Vos données : </h3> </br>
+
+<?php
+echo '<input type="hidden" name="login" value="'.$_SESSION['login'].'">';
+echo ' Votre login : '. $_SESSION['login'] .'</br>';
+echo ' Votre mot de passe : ' .$_SESSION['pwd'].'</br>';
+foreach ($resultats as $row)
+{
+	echo ' Votre numéro de rue :  '. $row['numRue']. '</br>
+	 	   Votre rue : ' .$row['rue'] .'</br>
+	 	   Votre ville : '.$row['ville'].'</br>
+	 	   Votre code postal : ' .$row['codePostal'].'</br>
+       Votre numéro habilitation : '. $row['numHabilitation'].'</br>';
+}
+
+//echo 'Modifier vos informations concernant votre adresse en cliquant <a href = modificationAdresse> ici </a>';
+?>
+
+<style type="text/css">
+.footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+           }
+#logoCompte {
+  color: #000000;
+  margin-right: 20px;
+}
+</style>
+
+<?php
+?>
+</br>
+<!--<h2> Modification de l'adresse : </h2>-->
+<h4> Veuillez renseigner les champs ci-dessous si vous voulez modifier votre adresse </h4> <br></br>
+<form method="post" action="<?php echo site_url ('utilisateur/modificationNumRue'); ?>">
+                <p>
+          <label for="numRue">Votre nouveau numéro de rue: </label>
+                <input type="text" name="numRue"/> &nbsp &nbsp
+                <input class="btn btn-large btn-primary" type="submit" value="Valider" />
+                <input class="btn btn-large btn-primary" type="reset" value="Effacer" /><br></br>
+                </p>
+</form>
+<form method="post" action="<?php echo site_url ('utilisateur/modificationRue'); ?>">
+                <p>
+                    <label for="rue">Votre nouvelle rue : </label>
+                    <input type="text" name="rue"/> &nbsp &nbsp
+                    <input class="btn btn-large btn-primary" type="submit" value="Valider" />
+                    <input class="btn btn-large btn-primary" type="reset" value="Effacer" /><br></br>
+                </p>
+</form>
+<form method="post" action="<?php echo site_url ('utilisateur/modificationVille'); ?>">
+                <p>
+
+                    <label for="ville">Votre nouvelle ville : </label>
+                    <input type="text" name="ville"/> &nbsp &nbsp
+                    <input class="btn btn-large btn-primary" type="submit" value="Valider" />
+                    <input class="btn btn-large btn-primary" type="reset" value="Effacer" /><br></br>
+                </p>
+</form>
+<form method="post" action="<?php echo site_url ('utilisateur/modificationCodePostal'); ?>">
+                <p>
+                    <label for="codePostal">Votre nouveau code postal : </label>
+                    <input type="text" name="codePostal"/> &nbsp &nbsp
+                    <input class="btn btn-large btn-primary" type="submit" value="Valider" />
+                    <input class="btn btn-large btn-primary" type="reset" value="Effacer" /><br></br>
+                </p>
+</form>
+<form method="post" action="<?php echo site_url ('utilisateur/modificationNumHabilitation'); ?>">
+                <p>
+                    <label for="numHabilitation">Votre nouveau numéro d'habitation : </label>
+                    <input type="text" name="numHabilitation"/> &nbsp &nbsp
+                    <input class="btn btn-large btn-primary" type="submit" value="Valider" />
+                    <input class="btn btn-large btn-primary" type="reset" value="Effacer" />
+                </p>
+</form>
+                
