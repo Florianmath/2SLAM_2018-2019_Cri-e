e512d145759050b0f67fd699e3f706347dbbce91
Julien-Treuschel <julien.treuschel@gmail.com>
2019-02-11 13:58:36 +0100

    patch-3.2
    Ajout du css pour les tableaux

--- views/v_accueil.php
+++ views/v_accueil.php
@@ -1,22 +1,50 @@
-<!-- Accueil-->
+<!-- Accueil
 <section>
-			<h1 id="titre">"Dites-le avec Lafleur" </h1>
-			
-			<!-- Pour l'image-->
-			<p><img src="<?php echo base_url('Images/ACCUEIL.JPG');?>" class="imageflottante" alt="Image flottante"/> </p>
-			
-			<div>
-				<p>
-					Constituez votre panier en navigant, puis cliquez sur "Commander"
-				</p>
+			<h1 id="titre">DE LA MER AU PORT, ET DU PORT A L'ASSIETTE !</h1>
+-->
+			<!-- Pour le css-->
+
+<style type="text/css">
+.footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+           }
+#logoAcc {
+  color: #000000;
+  margin-right: 20px;
+}
+</style>
+</head>
+	<body>
+		<!-- bannière -->
+		<section class="container-fluid banner">
+			<div class="ban">
+				<video src="<?php echo base_url('Images/2.mp4');?>" type="video/mp4" autoplay="true" loop="true" muted="true" />
+			</div>
+			<div class="inner-banner">
+				<h1 id="titre">Bienvenue sur le site de la Criée</h1>
+				</br>
+				<h2 id="titre">DE LA MER AU PORT, ET DU PORT A L'ASSIETTE !</h2>
+				</br>
+				<input class="btn btn-custom" type="button" value="Voir nos produits !" onclick="document.location.href='<?php echo site_url ('utilisateur/contenu/catalogue'); ?>';">
 			</div>
 		</section>
-		
-		
-		<div>
-			<p>
-				Vous devez être recensé comme client pour pouvoir commander.</br>
-				Envoyez un mail en laissant vos coordonnées pour être contacté par notre
-											service commercial.
+
+			<p> Ismaelito</p>
+
+
+
+
+
+
+
+<!--
+<footer class="container-fluid footer">
+		<div class="container">
+			<p>Vous devez être recensé comme client pour pouvoir commander.</br>
+				Envoyer un mail en laissant vos coordonnées pour être contacté par notre service commercial.
 			</p>
 		</div>
+</footer>-->
